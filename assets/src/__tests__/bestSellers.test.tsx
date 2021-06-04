3d041e1c273717b7480ba0d2c969d13c689c3a9f
Villavicencio, Lewis Deinel Banaag(Lewis) <lewis16.v@gmail.com>
2021-06-04 15:09:44 +1200

    Fixed errors and added tests for pastPurchases

--- assets/src/__tests__/bestSellers.test.tsx
+++ assets/src/__tests__/bestSellers.test.tsx
@@ -5,14 +5,11 @@ import BestSellers from '../modules/bestSellers/BestSellers';
 window.alert = jest.fn();
 test('Heading for best sellers component should show text correctly', function () {
     render(<BestSellers />);
-    
     let heading = document.getElementsByClassName("container-category");
     const windowAlert = window.alert;
-    
     if(heading != null)
     {
         expect(heading[0].getElementsByTagName('h3')[0].innerHTML).toBe("Top 20 best sellers");
     }
     window.alert = windowAlert;
-    
 });
--- assets/src/__tests__/pastPurchases.test.tsx
+++ assets/src/__tests__/pastPurchases.test.tsx
@@ -11,3 +11,14 @@ test('renders without crashing', () => {
     ReactDOM.unmountComponentAtNode(div);
     window.alert = windowAlert;
 });
+
+test('Heading for best sellers component should show text correctly', function () {
+    render(<PastPurchases />);
+    let heading = document.getElementsByClassName("white-box");
+    const windowAlert = window.alert;
+    if(heading != null)
+    {
+        expect(heading[0].getElementsByTagName('h3')[0].innerHTML).toBe("Past purchases");
+    }
+    window.alert = windowAlert;
+});
